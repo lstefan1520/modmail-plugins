bb64a0893f01eda81f57c2acd88e8de816d797b2
Mark David <44349634+markd69@users.noreply.github.com>
2020-04-12 11:17:48 -0400

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands, tasks, guild, context
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+
+    @client.command()
+    @commands.has_permissions(manage_roles=True)
+    async def say(self, ctx, *, arg):
+        await ctx.send(arg)
+
+
+def setup(bot):
+    bot.add_cog(MyCog(bot)) 
