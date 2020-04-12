593fc1127ed020caef0fdd4528619801799c134f
Mark David <44349634+markd69@users.noreply.github.com>
2020-04-12 11:09:11 -0400

    Create staff.py

---
+++ staff/staff.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands, tasks, guild
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
+    
+    @bot.event
+    # this is where it checks when a message is sent if the message is in one of the channels under waiting for staff and if the message sent is from a staff channel will be moved to waiting for client
+    async def on_message(self, message):
+        channel = message.channel
+        user = message.author
+        categoryitsin = discord.utils.get(guild.categories, id=537807191042949131)
+        categorytomoveto = discord.utils.get(guild.categories, id=667604143413788672)
+        if discord.utils.get(user.roles, name="Staff"):
+            if message.channel.category == categoryitsin:
+                channel.edit(category=categorytomoveto)
+                
+            
+
+
+
+        
+
+def setup(bot):
+    bot.add_cog(MyCog(bot))
