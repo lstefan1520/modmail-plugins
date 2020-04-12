0085149bfc5a66afb245bcb664af9e0b4f0305b3
Mark David <44349634+markd69@users.noreply.github.com>
2020-04-12 11:57:30 -0400

    Update staff.py

--- staff/staff.py
+++ staff/staff.py
@@ -19,11 +19,11 @@ class MyCog(commands.Cog):
     async def on_message(self, message):
         channel = message.channel
         user = message.author
-        categoryitsin = discord.utils.get(discord.Guild.categories, id=537807191042949131)
-        categorytomoveto = discord.utils.get(discord.Guild.categories, id=667604143413788672)
-        if discord.utils.get(user.roles, name="staff"):
-            if message.channel.category == categoryitsin:
-                channel.edit(category=categorytomoveto)
+        categoryitsin = discord.utils.get(537807191042949131) # here you put the category the message is already in (waiting for staff)
+        categorytomoveto = discord.utils.get(667604143413788672) # here you put the category you want the channel to get moved to (waiting for client)
+        if discord.utils.get(user.roles, name="staff"): # checks if a staff member sent that message (do not give staff rank to a bot)
+            if message.TextChannel.category_id == categoryitsin: # here it checks if the channel message was sent in the category (waiting for staff)
+                channel.edit(category=categorytomoveto) # here it moves it to (waiting for client)
                 
             
 
